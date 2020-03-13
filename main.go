0aa57a179f8a36436ab668301c7de00f8c4be2d4
unknown <gilsptr@gmail.com>
2020-03-13 14:13:43 +0700

    2

--- main.go
+++ main.go
@@ -62,6 +62,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homePage)
 	r.HandleFunc("/result", homeResult)
+	r.HandleFunc("/result2", homeResult)
 	/*
 		http.HandleFunc("/", homePage)
 		http.HandleFunc("/result/pokemon={id}", homePage)
